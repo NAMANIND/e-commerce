100472c6ffcbf2d164c261f1310c78549ae317a4
Naman rai <namanrai309@gmail.com>
2025-03-07 18:40:30 +0530

    hmm

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     domains: [
       "images.unsplash.com",
       "placehold.co",
-      "szovgbyhjonfpuklnmur.supabase.co",
+      "msbbxdjcsvosqijxlgbw.supabase.co",
     ],
   },
 };
